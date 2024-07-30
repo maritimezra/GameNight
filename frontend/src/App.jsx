f11e8d638625347aef151052f794e675d4b201d7
maritimezra <ezramaritim08@gmail.com>
2024-07-30 14:20:54 +0300

    Defined the App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,45 @@
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Home from './components/Home';
+import PrivateRoute from './components/PrivateRoute';
+import Tod from './components/TOD';
+import Layout from './components/Layout';
 
-function App() {
-  
-}
 
-export default App
+
+const httpLink = createHttpLink({
+  uri: 'http://127.0.0.1:8000/graphql/',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
+
+const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<PrivateRoute element={Home} />} />
+            <Route path="/play-game" element={<PrivateRoute element={Tod} />} />
+          </Routes>
+          </Layout>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+export default App;
