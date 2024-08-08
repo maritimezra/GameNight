d0c5f910f1e043376ffbcccdfe33dc6657cf5d1a
maritimezra <ezramaritim08@gmail.com>
2024-08-08 10:15:05 +0300

    Added an isAuthenticated prop for the profile icon

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,32 @@
 import PropTypes from 'prop-types';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
-
+import { useState } from 'react';
+import Login from './Login';
 
 const Header = ({ openProfileModal }) => {
   const navigate = useNavigate();
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
+  const [showLoginModal, setShowLoginModal] = useState(false);
 
   const handleProfileClick = () => {
-    openProfileModal();
+    if (isAuthenticated) {
+      openProfileModal();
+    } else {
+      setShowLoginModal(true);
+    }
   };
 
   const handleTitleClick = () => {
     navigate('/');
-  }
+  };
+
+  const handleLoginClose = () => {
+    setShowLoginModal(false);
+    setIsAuthenticated(!!localStorage.getItem('token'));
+  };
 
   return (
     <header className="header">
@@ -25,12 +36,13 @@ const Header = ({ openProfileModal }) => {
           <FontAwesomeIcon icon={faUser} />
         </span>
       </div>
+      {showLoginModal && <Login isOpen={true} onClose={handleLoginClose} />}
     </header>
   );
 };
 
 Header.propTypes = {
-    openProfileModal: PropTypes.func.isRequired,
-  };
+  openProfileModal: PropTypes.func.isRequired,
+};
 
 export default Header;
